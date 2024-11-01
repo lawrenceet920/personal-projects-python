2e2f252f7382b6189f938b8c5f1805fbd1762629
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-11-01 12:23:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 # Ethan Lawrence
 # Oct 9 2024
 # Choose your own Adventure
+import time
 
 hero_name = input('Old Man:     Hello adventurer and welcome to the kingdom of Chadlantus! \n Tell me, what is your name:   ')
-hero_class = input(f'Narrator:   {hero_name}, are you a mage, or a fighter? (input either "mage" or "fighter")  ')
-if hero_class == 'mage':
-    weapon = input(f'Narrator:   {hero_name}, what is your spellcasting focus? is it a wand, a staff, or something entirly unique?  ')
-elif hero_class == 'fighter':
-    weapon = input(f'Narrator:   {hero_name}, what is your weapon? is it a sword, a greataxe, or something entirly unique?')
-else:
-    print('OOPS Please input only mage or fighter!')
-print(f'Old Man:    Well then mightly {hero_name}, you have done well to Chadlantus! Now mighty {hero_class}, I need aid please deliver this letter to my daughter in the capital.')
+time.sleep(1)
 
+while hero_name != 2:
+    hero_class = input(f'Narrator:   {hero_name}, are you a mage, or a fighter? (input either "mage" or "fighter")  ')
+    if hero_class == 'mage':
+        weapon = input(f'Narrator:   {hero_name}, what is your spellcasting focus? is it a wand, a staff, or something entirly unique?  ')
+        break
+    elif hero_class == 'fighter':
+        weapon = input(f'Narrator:   {hero_name}, what is your weapon? is it a sword, a greataxe, or something entirly unique?')
+        break
+    else:
+        print('OOPS! Please input only mage or fighter!')
+time.sleep(1)
+print(f'Old Man:    Well then mightly {hero_name}, you have done well to Chadlantus! Now mighty {hero_class}, I need aid please deliver this letter to my daughter in the capital.')
+time.sleep(1)
 # River
 print('Narrator:    You set forth onto adventure, but suddenly you hit a roadblock, the bridge has collapsed!')
 if hero_class == 'mage':
@@ -19,10 +26,12 @@ if hero_class == 'mage':
     river = 'use levitation magic to cross a river'
 else: 
     river = input('Narrator:    How do you get across:      ')
-
+time.sleep(1)
 # Thug
 print('Narrator:    After passing the river you find yourself face to face with a thug!')
+time.sleep(1)
 print(f'Thug:   YOU! {hero_class} gimme that {weapon} or i\'ll... uh ... why are you standing like that give it!')
+time.sleep(1)
 if hero_class == 'fighter':
     print('Narrator:    You notice the incompitance of the thug and kick there leg and gesture threating them with your {weapon}, they can tell you far outclass them and run away.')
     thug = 'Scare away a thug'
@@ -31,6 +40,9 @@ else:
 
 # Locate daughter
 print('Narrator:    The thug dealt with you make your way to the capital, but you discover you forgot to ask where in the capital the Old man\'s daughter is')
+time.sleep(1)
 find_her = input('Narrator: You haven\'t time to leave and ask how do you try to find her?      ')
+time.sleep(1)
 print('You are now face to face with her, you hand her the old man\'s letter and have completed your quest')
+time.sleep(1)
 print(f'Tou complete your quest you had to {river}, then {thug}, and then locate the daughter by {find_her}. For these efforts you have been rewareded with 100 GP')
