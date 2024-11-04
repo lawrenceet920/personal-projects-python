5ef47b9e3d67cdb0b33b412208de359bbd8998d3
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-11-04 14:01:11 -0500

    Update roll-or-hold.py

--- roll-or-hold.py
+++ roll-or-hold.py
@@ -5,12 +5,15 @@
 import random
 import sys
 
-print('Hello, welcome to roll or hold, you will roll a 20 sided die, then will choose if you want to roll again to raise your number, or hold the number. If you can beat my score you win, if you go over 40 you lose. \n')
-input('Type anything to begin. \n')
+ai_score = 0
+score = 0
 
+print('Hello, welcome to roll or hold, you will roll a 20 sided die, then will choose if you want to roll again to raise your number, or hold the number. If you can beat my score you win, if you go over 40 you lose. \n')
+startup = input('Type anything to begin. \n')
+if startup == 'anything':
+    print('I hope you lose this round.')
 # Player Rolls
-score = random.randint(1, 20)
-while score < 40:
+while score < 41:
     reroll = input(f'Your current score is {score}, Roll again? input either y or n:     ').lower()
     if reroll in ['n', 'no']:
         break
@@ -21,17 +24,25 @@ while score < 40:
 else:
     sys.exit('You went over 40! Game over!')
 
+# already won?
+if score == 40:
+    print('You rolled a 40? You win!')
+
 # AI Rolls
 print(f'Your final score is {score}, My turn!')
-ai_score = 0
-while ai_score < 40:
+while ai_score < 41:
     print(f'My score is now {ai_score}')
     if ai_score > score:
         break
     ai_score += random.randint(1, 20)
 else:
-    print('Fiddlesticks! I pushed too far and rolled over 40.')
-    ai_score = 0
+    # If Then evilness
+    if startup == 'anything':
+        ai_score = 40
+        print(f'My score is now {ai_score}')
+    else:
+        print('Fiddlesticks! I pushed too far and rolled over 40.')
+        ai_score = 0
 
 # Final Score
 if score > ai_score:
