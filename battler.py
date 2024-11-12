69cc6fa92b610dfdf10445397019875631e6b378
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-11-12 13:30:55 -0500

    Combat (hopefully) done

--- battler.py
+++ battler.py
@@ -18,7 +18,8 @@ phase = 'combat'
 # Spells
 # Spell layout  0 name, 1description 2 min damage, 3 max damage, 4 total casts, 5 casts left, 6+ special effects
 spell1 = ['Firebolt', 'Your basic relyable attack, low damage', 5, 10, 100, 100]
-spell2 = ['Cure Wounds', 'heal yourself, meduim healing', 5, 10, 3, 3]
+spell2 = ['Cure Wounds', 'heal yourself, medium healing', 5, 10, 3, 3]
+spell3 = ['Wait', 'Do nothing at all']
 
 # Game loop
 while phase != 'game over':
@@ -27,14 +28,14 @@ while phase != 'game over':
         # Turn Decider
         initiative_total = player_stats[0] + monster_stats[0]
         turn_picker = random.randint(1, initiative_total) - player_stats[1] + monster_stats[1]
-        print('\n \n')
+        print('')
         if turn_picker < player_stats[0]:
             # player turn
             turn = 'player'
             # Give information
-            print(f'it is the {turn}\s turn.')
-            print(f'Your health is {player_stats[4]}/{player_stats[3]}')
-            print(f'{monster_stats[5]} health is {monster_stats[4]}/{monster_stats[3]}')
+            print(f'it is the {turn}\'s turn.')
+            print(f'Your health is {int(player_stats[4])}/{int(player_stats[3])}')
+            print(f'{monster_stats[5]} health is {int(monster_stats[4])}/{int(monster_stats[3])}')
             while turn == 'player':
                 print(f'{spell1[0]} : {spell1[1]}')
                 print(f'{spell2[0]} : {spell2[1]}')
@@ -43,32 +44,40 @@ while phase != 'game over':
                 if player_action == spell1[0]:
                     attack_damage = random.randint(spell1[2] + player_stats[1], spell1[3] + player_stats[1]) 
                     attack_damage += attack_damage * (player_stats[2] / 100)
-                    monster_stats[4] -= attack_damage
+                    monster_stats[4] -= int(attack_damage)
                     print(f'You did {attack_damage} damage to the monster!')
                     turn = 'stage'
                 elif player_action == spell2[0]:
                     attack_damage = random.randint(spell2[2] + player_stats[1], spell2[3] + player_stats[1]) 
                     attack_damage += attack_damage * (player_stats[2] / 100)
-                    player_stats[4] += attack_damage
+                    player_stats[4] += int(attack_damage)
                     print(f'You did {attack_damage} damage to the monster!')
+                    turn = 'stage'
+                elif player_action == spell3[0]:
+                    print('You stand there like a fool.')
+                    player_stats[0] = 1
+                    player_stats[1] = 0
                     turn = 'stage'
                 else:
                     print('Oops that wasn\'t an option try again (capitalisation matters)')
-                        # Health Check
-        if player_stats[4] < 1:
-            phase == 'game over'
-        elif monster_stats[4] < 1:
-            phase == 'battle won'
+            # End Of Player turn while
         else:
             # Monster turn
             turn = 'monster'
-            print(f'it is the {monster_stats[5]}\s turn.')
+            print(f'it is the {monster_stats[5]}\'s turn.')
             while turn == 'monster':
-                attack_damage = random.randint(monster_stats[4] + monster_stats[1], monster_stats[3] + monster_stats[1]) 
+                # (monster_stats[4] + monster_stats[1], monster_stats[3] + monster_stats[1])
+                attack_damage = random.randint(monster_stats[4], monster_stats[3]) 
                 attack_damage += attack_damage * (monster_stats[2] / 100)
-                player_stats[4] -= attack_damage
+                player_stats[4] -= int(attack_damage)
                 print(f'The monster attacks for {attack_damage} damage!')
                 turn = 'stage'
+        # Health Check
+        if player_stats[4] < 1:
+            phase = 'game over'
+        elif monster_stats[4] < 1:
+            phase = 'battle won'
+    # End Of combat While statement
     while phase == 'battle won':
         print('You won the battle!')
         phase = 'shop'
